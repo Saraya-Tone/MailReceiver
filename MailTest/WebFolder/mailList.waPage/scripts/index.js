7c027a56f4c04854becae6b3ba77013ef12746f1
WebApp64 <tone@saraya.com>
2013-05-14 10:56:04 +0900

    Update #9 保存未完フィルター追加

--- MailTest/WebFolder/mailList.waPage/scripts/index.js
+++ MailTest/WebFolder/mailList.waPage/scripts/index.js
@@ -2,6 +2,7 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var checkbox1 = {};	// @checkbox
 	var documentEvent = {};	// @document
 	var saveFiles = {};	// @button
 	var getNewMail = {};	// @button
@@ -9,10 +10,24 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 // eventHandlers// @lock
 
+	checkbox1.click = function checkbox1_click (event)// @startlock
+	{// @endlock
+		if ($$('checkbox1').getValue() == true ) 
+		{
+			sources.mailbox.query("allSaved == false", {orderBy:"dateString desc"});	
+//			sources.mailbox.orderBy("dateString desc");						
+		} 
+		else
+		{
+			sources.mailbox.query("allSaved == false OR allSaved == true",{orderBy:"dateString desc"});	
+		};		
+	};// @lock
+
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
 //		var id = setInterval(sources.mailbox.getNewMailCollection, 3*60*1000);  // 一時的にコメントアウト　本番時に使用		
 		$$('dataGrid1').setSortIndicator(1,"desc");　// 送信日付降順ソートを表示
+
 	};// @lock
 
 	saveFiles.click = function saveFiles_click (event)// @startlock
@@ -35,6 +50,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("checkbox1", "click", checkbox1.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("saveFiles", "click", saveFiles.click, "WAF");
 	WAF.addListener("getNewMail", "click", getNewMail.click, "WAF");
